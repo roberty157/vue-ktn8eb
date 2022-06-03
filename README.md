7402b5d5e456419b7df91426f3444595173ca7c1
roberty157 <robertyeam@gmail.com>
2022-06-03 12:44:51 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-ktn8eb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-ktn8eb)
---
+++ src/components/Tabs.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <div>
+    <ul class="tabs__header">
+      <li v-for="tab in tabs" :key="tab.title">{{ tab.title }}</li>
+    </ul>
+    <slot></slot>
+  </div>
+</template>
+
+
+<script>
+  export default {
+    name:'Tabs',
+    data () {
+      return {
+        selectedIndex: 0 // the index of the selected tab,
+        tabs: [], // all of the tabs
+      },
+      created () {
+        this.tabs = this.$children
+      }
+    }
+  }
+</script>
